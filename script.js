366360490700a450fdadfc17158029507477afcc
Tom - Linux Desktop <tomd304@live.co.uk>
2021-12-01 19:37:41 +0000

    Created html page to play game, and updated JS file to link to html

--- script.js
+++ script.js
@@ -3,9 +3,9 @@ function computerPlay() {
     return rand >= 2 ? "rock" : rand >= 1 ? "paper" : "scissors";
 }
 
-function playRound(playerChoice, computerChoice) {
-    console.log(`player: ${playerChoice}`);
-    console.log(`computer: ${computerChoice}`);
+function playRound(playerChoice) {
+    let computerChoice = computerPlay();
+    updateIMG(playerChoice, computerChoice);
 
     switch (playerChoice) {
         case computerChoice:
@@ -19,18 +19,53 @@ function playRound(playerChoice, computerChoice) {
     }
 }
 
+function updateIMG(playerChoice, computerChoice) {
+    console.log(document.querySelector('#playerIMG').src = `${playerChoice}.png`)
+    console.log(document.querySelector('#computerIMG').src = `${computerChoice}.png`)
+}
+
+function init() {
+    document.querySelector('.result').style.display = 'flex'
+    document.querySelector('.scores').style.display = 'flex'
+}
 
 let winner;
+let winnerMsg;
 let playerScore = 0;
 let computerScore = 0;
 let drawCount = 0;
 
-for (let i = 0; i < 5; i++) {
-    let playerChoice = prompt("Rock, Paper or Scissors?").toLowerCase();
-    winner = playRound(playerChoice, computerPlay());
-    winner == "player" ? playerScore++ : winner == "computer" ? computerScore++ : drawCount++
+let btns = document.querySelectorAll('button')
+btns.forEach((btn) => {
+    btn.addEventListener('click', function(e) {
+        winner = playRound(e.target.id)
+        init()
+        updateStats()
+        updateDOM()
+    })
+});
+
+function updateStats() {
+    switch (winner) {
+        case 'player':
+            winnerMsg = 'Player Wins!'
+            playerScore++
+            break;
+        case 'computer':
+            winnerMsg = 'Computer Wins!'
+            computerScore++
+            break;
+        default:
+            winnerMsg = 'Draw!'
+            drawCount++
+            break;
+    }
 }
 
-console.log(`Player wins: ${playerScore}`);
-console.log(`Computer wins: ${computerScore}`);
-console.log(`Draws: ${drawCount}`);
+function updateDOM() {
+    document.querySelector('.winner').textContent = winnerMsg
+    document.querySelector('#playerScore').textContent = playerScore
+    document.querySelector('#computerScore').textContent = computerScore
+    document.querySelector('#drawCount').textContent = drawCount
+    
+}
